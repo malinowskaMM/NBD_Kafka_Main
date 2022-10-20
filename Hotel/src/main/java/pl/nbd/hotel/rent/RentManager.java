7da0ff2d518f724370ee2845e91ecbe4bac3649e
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2022-10-20 19:10:44 +0200

    fix

--- Hotel/src/main/java/pl/nbd/hotel/rent/RentManager.java
+++ Hotel/src/main/java/pl/nbd/hotel/rent/RentManager.java
@@ -62,6 +62,7 @@ public class RentManager {
             if(rent1 != null) {
                 Client client = rent1.getClient();
                 client.setMoneySpent(rent1.client.getMoneySpent() + rent1.rentCost);
+                entityManager.lock(rent1.getRoom(), LockModeType.NONE);
                 rentRepository.remove(rent1);
                 checkChangeClientType(client);
                 entityManager.getTransaction().commit();
